46e73e0acfd4d1f9cf18f4e4866a78d512d184f5
Subhadra <117344735+raisubhadra@users.noreply.github.com>
2022-12-15 22:33:47 +0530

    Add files via upload

---
+++ 15-12-2022/SearchLetterLab2.java
@@ -0,0 +1,21 @@
+//wap to check if the worrd "I" is present in the "I love my India" or not.
+package oopsconcept;
+import java.util.Scanner;
+public class SearchLetterLab2 
+{
+	public static void main(String[] args) {
+		String a;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your input here: "); // I love my India
+		a = sc.nextLine();
+
+		// Checks if "I" is present in the given String
+
+		if (((a.startsWith("i"))) || ((a.startsWith("I"))))
+			System.out.println("The letter is present.");
+		else
+			System.out.println("No such input can be found");
+
+	}
+}
